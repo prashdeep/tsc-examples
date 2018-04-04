518796f02e303002bac62301d6b4aca411b9c6e6
pradeep.kumar44@gmail.com <pradeep.kumar44@gmail.com>
2018-04-04 06:52:11 +0530

    first commit for angular4.

---
+++ types.ts
@@ -0,0 +1,9 @@
+let numberType:number;
+let strType:string;
+let booleanType:boolean;
+let arrayType:any[];
+let initializeArray:number[]=[1,2,3,4];
+
+enum Options{DEPOSIT, CHECK_BALANCE, WITHDRAW};
+
+let option:Options= Options.CHECK_BALANCE;
